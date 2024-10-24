d2d42058d648936ee9ab8ef2cd76bd477f8064b7
bhargav1997 <sutharbhargav1997@gmail.com>
2024-10-24 18:37:46 -0400

    test

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -34,6 +34,7 @@ const Login: React.FC = () => {
           <input
             type="text"
             id="username"
+            placeholder='Enter your username'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -47,6 +48,7 @@ const Login: React.FC = () => {
           <input
             type="password"
             id="password"
+            placeholder='Enter your Password..'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
